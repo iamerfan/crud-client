8869b439e764a894dd8c4de1fa2038cb3cc2cf30
Erfan <erfanhosseinizadeh@gmail.com>
2023-10-25 23:25:29 +0330

    sf

--- const.js
+++ const.js
@@ -1,4 +1,4 @@
 export const server =
   process.env.SERVER || process.env.NODE_ENV === "production"
-    ? "https://crud-server-iamerfan.vercel.app/api/items"
+    ? "https://splendid-hosiery-bass.cyclic.app/api/items"
     : "http://localhost:3001/api/items";
